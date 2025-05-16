7303fbaf36866c1ddab2130da6db4f3412117d68
Anthony.Preucil <Anthony.Preucil@noaa.gov>
2025-05-16 11:16:27 -0400

    using f2py? Maybe?

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-from setuptools import setup, find_packages, Extension
-import numpy
+# filepath: /ncrc/home2/Anthony.Preucil/apscratch/HydroBlocks/geospatialtools/setup.py
+from setuptools import setup, find_packages
+from numpy.distutils.core import Extension, setup
 
 extensions = [
     Extension(
         name="geospatialtools.terrain_tools_fortran",
         sources=["src/planchon_2001.f90", "src/terrain_tools.f90"],
-        extra_compile_args=["-fPIC", "-Wall", "-pedantic", "-O3"],
-        include_dirs=[numpy.get_include()],
+        extra_f90_compile_args=["-fPIC", "-Wall", "-pedantic", "-O3"],
     ),
     Extension(
         name="geospatialtools.upscaling_tools_fortran",
         sources=["src/upscaling_tools.f90"],
-        extra_compile_args=["-fPIC", "-Wall", "-pedantic", "-O3"],
-        include_dirs=[numpy.get_include()],
+        extra_f90_compile_args=["-fPIC", "-Wall", "-pedantic", "-O3"],
     ),
 ]
 
